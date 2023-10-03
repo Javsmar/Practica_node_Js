b2af7f989797fe4f198c1451741eb01794770051
Javier <mastos44@gmail.com>
2023-10-03 14:55:31 +0200

    express-validator, gitignore

--- Nodepop/routes/index.js
+++ Nodepop/routes/index.js
@@ -62,7 +62,5 @@ router.get('/', function(req, res, next) {
 });
 
 
-
-
 module.exports = router;
 
--- Nodepop/routes/users.js
+++ Nodepop/routes/users.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
+
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('App en desarrollo');
 });
+
+router.post('/',(req, res, next) => {
+  console.log(req.body);
+  res.send('recibido')
+})
 
 module.exports = router;

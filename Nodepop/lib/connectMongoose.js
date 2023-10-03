5dae28d1c6c0c497c3e02b341df15e4702136859
Javier <mastos44@gmail.com>
2023-10-03 19:17:10 +0200

    instalación y conección de mongoose

---
+++ Nodepop/lib/connectMongoose.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+mongoose.connection.on('error', err => {
+    console.log('Error de conexión', err)
+});
+
+mongoose.connection.once('open', () => {
+    console.log('Conectado en mongodb en', mongoose.connection.name);
+})
+
+mongoose.connect('mongodb://127.0.0.1/dbnodepop')
